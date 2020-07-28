063f67b2dbaf3777d8949acc1037cf2dc0c4f4cd
M. Fatih MAR <mfatihmar@gmail.com>
2020-07-28 16:19:14 +0100

    Implement Address and Browser Parsing for Pixel

--- api/pixel.go
+++ api/pixel.go
@@ -1,7 +1,12 @@
 package api
 
 import (
+	"log"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/mssola/user_agent"
 )
 
 // 1 pixel transparent GIF in 43 bytes
@@ -11,7 +16,42 @@ var gif = []byte{
 	0x01, 0x00, 0x01, 0x00, 0x00, 0x02, 0x02, 0x44, 0x01, 0x00, 0x3B,
 }
 
+func getTimeUTC() string {
+	now := time.Now().UTC()
+	unix := now.Unix()
+	return now.Format("2 Jan 2006, 3:04 PM") + " / " + strconv.FormatInt(unix, 10)
+}
+
+func getAddressFromReq(r *http.Request) string {
+	xff := r.Header.Get("X-Forwarded-For")
+	if xff != "" {
+		return "xff-" + xff
+	}
+
+	return r.RemoteAddr
+}
+
+func getBrowserFromReq(r *http.Request) string {
+	ua := user_agent.New(r.Header.Get("User-Agent"))
+	os := ua.OSInfo().Name
+	bot := ua.Bot()
+	mobile := ua.Mobile()
+	browser, _ := ua.Browser()
+	browser = browser + " / " + os
+	if bot {
+		browser = "[BOT] " + browser
+	}
+	if mobile {
+		browser = browser + " (mobile)"
+	}
+	return browser
+}
+
 func pixel(w http.ResponseWriter, r *http.Request) {
+	log.Println("timeutc:", getTimeUTC())
+	log.Println("address:", getAddressFromReq(r))
+	log.Println("browser:", getBrowserFromReq(r))
+
 	w.Header().Set("Cache-Control", "max-age=0, no-cache, no-store, must-revalidate")
 	w.Header().Set("Pragma", "no-cache")
 	w.Header().Set("Expires", "0")
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module mfatihmar
 
 go 1.14
+
+require github.com/mssola/user_agent v0.5.2
